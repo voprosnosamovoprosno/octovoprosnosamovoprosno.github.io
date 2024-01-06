c9078931b6835b96ebfe334c964ed0b9efb6a1ee
voprosnosamovoprosno <126585370+voprosnosamovoprosno@users.noreply.github.com>
2024-01-06 17:10:23 +0200

    Update vss.js

--- vss.js
+++ vss.js
@@ -1,1 +1,14 @@
-console.log("?вопросносамовопросно?");
+const http = require("http");
+
+const host = 'localhost';
+const port = 8000;
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end("?вопросносамовопросно?");
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`?вопросносамовопросно? http://${host}:${port}`);
+});
