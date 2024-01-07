452a2c084ad7c258dd4101891cdf2b2bbba947b6
voprosnosamovoprosno <126585370+voprosnosamovoprosno@users.noreply.github.com>
2024-01-07 14:44:55 +0200

    Update vss.js

--- vss.js
+++ vss.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const fs = require('fs').promises;
 
-const host = 'localhost';
+const host = '0.0.0.0';
 const port = 10000;
 
 const requestListener = function (req, res) {
