87574f25266daf595a3ed8574db795b33a4cdb07
Glitch (octovoprosnosamovoprosno) <none>
2024-02-10 17:39:54 +0000

    ?вопросносамовопросно?

--- server.js
+++ server.js
@@ -1,122 +1,87 @@
-/**
- * This is the main server script that provides the API endpoints
- *
- * Uses sqlite.js to connect to db
- */
 const path = require("path");
-
 const fastify = require("fastify")({
   // Set this to true for detailed logging:
   logger: false
 });
-
 // Setup our static files
 fastify.register(require("@fastify/static"), {
   root: path.join(__dirname, "public"),
   prefix: "/", // optional: default '/'
 });
-  
 fastify.register(require("@fastify/formbody"));
-
 // point-of-view is a templating manager for fastify
 fastify.register(require("@fastify/view"), {
   engine: {
     handlebars: require("handlebars"),
   },
 });
-
 const db = require("./sqlite.js");
 const errorMessage =
   "Whoops! Error connecting to the database–please try again!";
-
 // OnRoute hook to list endpoints
 const routes = { endpoints: [] };
 fastify.addHook("onRoute", routeOptions => {
   routes.endpoints.push(routeOptions.method + " " + routeOptions.path);
 });
 
-// Just send some info at the home route
+var vssgml=null;
+async function vssmsf()
+   {let vssbvl=false;
+    vssgml = await db.vssgetf();
+    console.log('?vssgml.length'+vssgml.length+'?');
+    for(let vssj=0;vssj<vssgml.length;vssj++)
+      {if(vssj<vssgml.length-12)
+        {vssdml=await db.vssdelf(vssgml[vssj].vssid);
+         vssbvl=true;
+         console.log('?vssqntdelid'+vssgml[vssj].vssid+vssdml+'?');
+        }else{if(vssgml[vssj].vsstxt[0]!='?')
+               {vssdml=await db.vssdelf(vssgml[vssj].vssid);
+                vssbvl=true;
+                console.log('?vsscntdelid'+vssgml[vssj].vssid+vssdml+'?');
+               }
+             }
+      }   
+    if(vssbvl==true)
+     {vssgml = await db.vssgetf();
+      console.log('?vssgml.length'+vssgml.length+'?');
+     }
+  }
+fastify.post("/", async (request, reply) => {
+ if(vssgml==null){await vssmsf();}
+ if(request.body!=-1)
+  {for(vssi=vssgml.length-1;vssi>0;vssi--)
+    {if(vssgml[vssi].vssid==request.body)
+      {console.log('?vssgml[vssi].vssid'+vssgml[vssi-1].vssid+'?');
+       return vssgml[vssi-1].vsstxt+vssgml[vssi-1].vssid;
+   }}}
+  if(vssgml.length-1>=0)
+    {console.log('?vssgml[vssgml.length-1].vssid'+vssgml[vssgml.length-1].vssid+'?');
+     return vssgml[vssgml.length-1].vsstxt+vssgml[vssgml.length-1].vssid;
+    }
+});
+fastify.post("/vssadd", async (request, reply) => {
+  let vssaml = await db.vssaddf(request.body);
+  console.log('?vssadd'+vssaml+'?');
+  console.log(request.body);
+  vssmsf();
+});
+fastify.get("/vssxs", async (request, reply) => {
+  console.log('?vssxs?');
+});
 fastify.get("/", (request, reply) => {
- /* const data = {
-    title: "Hello SQLite (blank)",
-    intro: "This is a database-backed API with the following endpoints",
-    routes: routes.endpoints
-  };
-  reply.status(200).send(data);
-*/
   return reply.view('voprosnosamovoprosno.html');
 });
-
-function randomInteger(min, max) {
-  // случайное число от min до (max+1)
-  let rand = min + Math.random() * (max + 1 - min);
-  return Math.floor(rand);
-}
-
-fastify.post("/", async (request, reply) => {
-  console.log(request.body);
-  if(request.body=='?вопросносамовопросно?')
-    {let vssgml = await db.getMessages();
-     console.log('?vssmsgsqnt'+vssgml.length+'?');
-    
-    if(vssgml.length>812)
-       {for(let vssj=0;vssj<vssgml.length-12;vssj++)
-         {vssdml=await db.deleteMessage(vssgml[vssj].id);
-          console.log('?vssdelmesid'+vssgml[vssj].id+vssdml+'?');
-         }
-        vssgml = await db.getMessages();
-        console.log('?vssmsgsqnt'+vssgml.length+'?'); 
-       }
-            
-     let vssmsl=[];
-     for(let vssi=0;vssi<vssgml.length;vssi++)
-       {if(vssgml[vssi].message.substring(0,1)=='?')
-         {vssmsl.push(vssgml[vssi].message);
-         }else{vssdml=await db.deleteMessage(vssgml[vssi].id);
-              console.log('?vssdelmesid'+vssgml[vssi].id+vssdml+'?');
-              }      
-       }        
-   //console.log('?vssmessages?'+'\n'+vssmsl);
-     let vssri=randomInteger(0,vssmsl.length-1);
-     console.log('?vssrandom'+vssri+'?');
-     return vssmsl[vssri];
-    }
-   else{let vssaml = await db.addMessage(request.body);
-        console.log('?vssaddmessage'+vssaml+'?');
-        return request.body;
-       }
+fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`?vss${address}?`);
 });
-
-// Return the chat messages from the database helper script - no auth
-fastify.get("/messages", async (request, reply) => {
-  let data = {};
-  data.chat = await db.getMessages();
-  console.log(data.chat);
-  if(!data.chat) data.error = errorMessage;
-  const status = data.error ? 400 : 200;
-  reply.status(status).send(data);
-});
-
-// Add new message (auth)
-fastify.post("/message", async (request, reply) => {
-  let data = {};
-  const auth = authorized(request.headers.admin_key);
-  if(!auth || !request.body || !request.body.message) data.success = false;
-  else if(auth) data.success = await db.addMessage(request.body.message);
-  const status = data.success ? 201 : auth ? 400 : 401;
-  reply.status(status).send(data);
-});
-
+/*
 // Update text for an message (auth)
 fastify.put("/message", async (request, reply) => { 
-  let data = {};
-  const auth = authorized(request.headers.admin_key);
-  if(!auth || !request.body || !request.body.id || !request.body.message) data.success = false;
-  else data.success = await db.updateMessage(request.body.id, request.body.message); 
-  const status = data.success ? 201 : auth ? 400 : 401;
-  reply.status(status).send(data);
 });
-
 // Delete a message (auth)
 fastify.delete("/message", async (request, reply) => {
   let data = {};
@@ -126,7 +91,6 @@ fastify.delete("/message", async (request, reply) => {
   const status = data.success ? 201 : auth ? 400 : 401;
   reply.status(status).send(data);
 });
-
 const vsskvn='vsskvv';
 // Helper function to authenticate the user key
 const authorized = key => {
@@ -139,12 +103,5 @@ const authorized = key => {
     return false;
   else return true;
 };
-
+*/
 // Run the server and report out to the logs
-fastify.listen({port:process.env.PORT, host:'0.0.0.0'}, function(err, address) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-  }
-  console.log(`?vss${address}?`);
-});
